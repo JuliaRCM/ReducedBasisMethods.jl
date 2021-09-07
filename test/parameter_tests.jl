02c7eaadd8cf813460468b53acee34492b5af8de
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-09-07 14:03:29 +0200

    Put ParameterSpace tests into separate file from Parameter tests.

--- test/parameter_tests.jl
+++ test/parameter_tests.jl
@@ -1,11 +1,5 @@
 
 samples = [0.0, 1.0, 2.0, 3.0, 4.0]
-grid    = [ 0.0  1.0  0.0
-            0.5  1.0  0.0
-            1.0  1.0  0.0
-            0.0  1.0  4.0
-            0.5  1.0  4.0
-            1.0  1.0  4.0 ]
 
 @testset "Parameter" begin
 
@@ -71,43 +65,5 @@ grid    = [ 0.0  1.0  0.0
     p3 = Parameter(:σ, 0.0, 4.0, 2)
 
     @test NamedTuple(p1, p2, p3) == NamedTuple{(:μ, :ν, :σ)}((p1, p2, p3))
-
-end
-
-@testset "ParameterSpace" begin
-
-    μ = Parameter(:μ, 0.0, 1.0, 3)
-    ν = Parameter(:ν, 1.0, 1.0, 1)
-    σ = Parameter(:σ, 0.0, 4.0, 2)
-
-    pkeys = (:μ, :ν, :σ)
-    params = (μ, ν, σ)
-
-    p1 = ParameterSpace(NamedTuple(params...), sample(CartesianParameterSampler(), params...))
-    p2 = ParameterSpace(NamedTuple(params...))
-    p3 = ParameterSpace(params...)
-
-    @test p1 == p2 == p3
-    @test p1[:] == p2[:] == p3[:] == grid
-    @test p1[:,:] == p2[:,:] == p3[:,:] == grid
-
-    for i in axes(grid,1)
-        @test p1(i) == p2(i) == p3(i) == NamedTuple{pkeys}(Tuple(grid[i,:]))
-        @test p1[i] == p2[i] == p3[i] == grid[i,:]
-        @test p1[i,:] == p2[i,:] == p3[i,:] == grid[i,:]
-    end
-
-    for j in axes(grid,2)
-        @test p1[:,j] == p2[:,j] == p3[:,j] == grid[:,j]
-        @test p1[:,pkeys[j]] == p2[:,pkeys[j]] == p3[:,pkeys[j]] == grid[:,j]
-        @test p1[pkeys[j]] == p2[pkeys[j]] == p3[pkeys[j]] == grid[:,j]
-    end
-
-    for i in axes(grid,1)
-        for j in axes(grid,2)
-            @test p1[i,j] == p2[i,j] == p3[i,j] == grid[i,j]
-            @test p1[i,pkeys[j]] == p2[i,pkeys[j]] == p3[i,pkeys[j]] == grid[i,j]
-        end
-    end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "ReducedBasisMethods.jl" begin
     include("parameter_tests.jl")
+    include("parameterspace_tests.jl")
 end
