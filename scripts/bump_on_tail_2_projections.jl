027739b25d01172c525ca8ffa162f5b6d2d6259a
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-04-27 18:33:33 +0200

    Clean up projection script and move code out of global scope.

--- scripts/bump_on_tail_2_projections.jl
+++ scripts/bump_on_tail_2_projections.jl
@@ -42,31 +42,47 @@ P₀ = ParticleList(X[:,1], V[:,1], ones(IP.nₚ) .* poisson.L ./ IP.nₚ)
 
 # EVD
 
-# XV = hcat(X, V);
-XV = copy(X)
-for p in 1:IP.nparam
-    # XV = hcat(XV, V[:,1+(p-1)*IP.nₛ])
-    global XV = hcat(XV, V[:,1+(p-1)*IP.nₛ]) #, E[:,p*IP.nₛ])
-    #print(1+(p-1)*IP.nₛ, " ", p*IP.nₛ, "\n" )
+function get_ΛΩ(fpath, IP; tolerance = 1e-4, k = 0)
+     X = h5read(fpath, "snapshots/X")
+     V = h5read(fpath, "snapshots/V")
+
+     XV = X
+     for p in 1:IP.nparam
+          # XV = hcat(XV, V[:,1+(p-1)*IP.nₛ])
+          XV = hcat(XV, V[:,1+(p-1)*IP.nₛ]) #, E[:,p*IP.nₛ])
+          #print(1+(p-1)*IP.nₛ, " ", p*IP.nₛ, "\n" )
+     end
+
+     # XV = hcat(X, V);
+
+     @time F = eigen(XV' * XV)
+     @time Λ, Ω = sorteigen(F.values, F.vectors)
+
+     # Projection Matrices
+     @time k, Ψ = get_Ψ(XV, Λ, Ω, tolerance, k)
+
+     return Λ, Ω, k, Ψ
 end
 
-# clear 
-X, V = 0, 0
-GC.gc()
+function get_ΛΩe(E; tolerance = 1e-4, k = 0)
+     @time Fₑ = eigen(E' * E)
+     @time Λₑ, Ωₑ = sorteigen(Fₑ.values, Fₑ.vectors)
+     
+     # Projection Matrices
+     @time kₑ, Ψₑ = get_Ψ(E, Λₑ, Ωₑ, tolerance, k)
+
+     return Λₑ, Ωₑ, kₑ, Ψₑ
+end
 
 
-# @time F = eigen(Xₑₓₜ' * Xₑₓₜ)
-# Λ, Ω = sorteigen(F.values, F.vectors)
-@time F = eigen(XV' * XV)
-@time Λ, Ω = sorteigen(F.values, F.vectors)
+Λ, Ω, k, Ψ = get_ΛΩ(fpath, IP)
 
-@time Fₑ = eigen(E' * E)
-@time Λₑ, Ωₑ = sorteigen(Fₑ.values, Fₑ.vectors)
+Λₑ, Ωₑ, kₑ, Ψₑ = get_ΛΩe(h5read(fpath, "snapshots/E"))
 
+# Λₑₓₜ, Ωₑₓₜ, kₑₓₜ, Ψₑₓₜ = get_ΛΩe(Xₑₓₜ)
 
 # clear
-F, Fₑ = 0, 0
-GC.gc()
+# GC.gc()
 
 
 # plot
@@ -81,16 +97,6 @@ plot(xlabel = L"$i$", ylabel = L"$\lambda_i$", yscale = :log10,
      grid = true, gridalpha = 0.5, legend = :none)
 plot!(abs.(Λ ), linewidth = 2, alpha = 0.25, label = L"$X_v$")
 plot!(abs.(Λₑ), linewidth = 2, alpha = 0.5,  label = L"$E$")
-
-
-# Projection Matrices
-@time k, Ψ = get_Ψ(XV, Λ, Ω, 1e-9, 0)
-@time kₑ, Ψₑ = get_Ψ(E, Λₑ, Ωₑ, 1e-5, 0)
-
-# clear 
-XV, E = 0, 0
-Λ, Λₑ, Ω, Ωₑ = 0, 0, 0, 0
-GC.gc()
 savefig("../runs/$(runid)_SVDs_BoT_2.pdf")
 
 
