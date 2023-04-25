33487fa29c5288424c798c36a419c40a7171e8bb
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-04-25 13:31:13 +0200

    Fix makefile for docs.

--- docs/make.jl
+++ docs/make.jl
@@ -11,11 +11,11 @@ using Weave
 makedocs(;
     modules=[ReducedBasisMethods],
     authors="Tobias M. Blickhan, Michael Kraus, Tomasz M. Tyranoski",
-    repo="https://github.com/JuliaGNI/ReducedBasisMethods.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaRCM/ReducedBasisMethods.jl/blob/{commit}{path}#L{line}",
     sitename="ReducedBasisMethods.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://juliagni.github.io/ReducedBasisMethods.jl",
+        canonical="https://juliarcm.github.io/ReducedBasisMethods.jl",
         assets=String[],
     ),
     pages=[
@@ -25,5 +25,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/JuliaGNI/ReducedBasisMethods.jl",
+    repo="github.com/JuliaRCM/ReducedBasisMethods.jl",
 )
